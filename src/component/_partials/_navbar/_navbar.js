a1cf74668315d9b2b2dd1f48227711ad69bae520
HoctoPrism <nemexxix@gmail.com>
2022-09-15 21:47:50 +0200

    redux removed + private route + redirection

--- src/component/_partials/_navbar/_navbar.js
+++ src/component/_partials/_navbar/_navbar.js
@@ -2,6 +2,8 @@ import {AppBar, Box, Button} from "@mui/material";
 import {SwitchModeButton} from "../_theme/_switchModeButton";
 import {useEffect} from "react";
 import '../../../assets/css/component/_partials/_navbar.scss';
+import {LogginButton} from "../../../services/auth/logginButton";
+import auth from "../../../services/auth/token"
 
 export function Navbar() {
 
@@ -14,6 +16,12 @@ export function Navbar() {
                 <Box sx={{m: 5, flexGrow: 1}} component="div">{document.title}</Box>
                 <Box className="navbar">
                     <Button color="secondary" href='/'>Accueil</Button>
+                    {auth.loggedAndAdmin() ? (
+                        <Box>
+                            <Button color="secondary" href='type'>Type</Button>
+                        </Box>
+                    ) : null }
+                    <LogginButton/>
                     <SwitchModeButton/>
                 </Box>
             </AppBar>
--- src/component/home/home.js
+++ src/component/home/home.js
@@ -1,8 +1,9 @@
 import React from "react";
 import {Box, Container, Typography} from "@mui/material";
+import AdminMessage from "../../services/auth/adminMessage";
 
 
-function Home() {
+function Home(props) {
 
     document.title = 'Page d\'accueil'
 
@@ -11,6 +12,9 @@ function Home() {
             <Typography variant='h2'>Welcome</Typography>
             <Typography variant='h5'>REGLER LE SOUCIS DE FLASH BLANC LORS D'UN LOAD DE PAGE</Typography>
         </Box>
+        {props.adminMessage ? (
+            <AdminMessage adminMessage={props.adminMessage}/>
+        ) : null}
     </Container>
 }
 
